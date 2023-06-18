7e88ca7db1e04d6589aec2c18a11b8943f6d0a7b
Nikolay Khvan <hwang.the.human@gmail.com>
2023-06-18 22:41:33 +0600

    add: timeout

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const db = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
+    acquireConnectionTimeout: 300000,
   },
 })
 
