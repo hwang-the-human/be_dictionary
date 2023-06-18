f41f98aeb423481f180e8ccca746b69ef7ac4083
Nikolay Khvan <hwang.the.human@gmail.com>
2023-06-18 22:46:00 +0600

    fix:

--- index.js
+++ index.js
@@ -25,7 +25,18 @@ const db = require("knex")({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
-    acquireConnectionTimeout: 300000,
+    acquireConnectionTimeout: 1000000,
+    pool: {
+      min: 0,
+      max: 4,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 2000,
+    },
+    debug: false,
   },
 })
 
