3203817993d1d6adcf3f6d160eff8772ebff30f3
Nikolay Khvan <hwang.the.human@gmail.com>
2023-06-19 10:29:58 +0600

    add heroku config

--- index.js
+++ index.js
@@ -15,33 +15,26 @@ const openai = new OpenAIApi(configuration)
 app.listen(port, () => console.log(`Listening on port ${port}...`))
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 
 const db = require("knex")({
-  client: process.env.DB_CLIENT,
+  client: "pg",
   connection: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_DATABASE,
-    acquireConnectionTimeout: 1000000,
-    pool: {
-      min: 100,
-      max: 10000,
-      createTimeoutMillis: 150000,
-      acquireTimeoutMillis: 150000,
-      idleTimeoutMillis: 600000,
-      reapIntervalMillis: 30000,
-      createRetryIntervalMillis: 30000,
-      propagateCreateError: false,
+    extra: {
+      ssl: true,
     },
-    debug: false,
   },
 })
 
 db.raw("SELECT 1")
   .then(() => {
+    createTable()
+    addMusicToDB()
     console.log("PostgreSQL connected.")
   })
   .catch((e) => {
